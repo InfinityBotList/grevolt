74303d43bad812876edc8c569c716f0a80983b3b
cheesycod <cheesycod@outlook.com>
2023-06-25 09:43:41 +0530

    more docs on Auth

--- auth/auth.go
+++ auth/auth.go
@@ -4,7 +4,13 @@ package auth
 // Token is a session token used for authorization
 type Token struct {
 	// Whether the token is a bot token
+	//
+	// Note: this is ignored on gateway, but required for making REST requests
+	//
+	// Also, note that preparing a websocket connection does require REST
+	// and so this field must be properly set anyways
 	Bot bool
+
 	// The session token
 	Token string
 }
