5915075ff9588a7cbf608e3e7d76b9d7d566bd2b
cheesycod <cheesycod@outlook.com>
2023-08-23 14:52:36 +0530

    clarify Multi docs

--- extras/advancedevents/multi.go
+++ extras/advancedevents/multi.go
@@ -14,19 +14,20 @@ func (m *Multi[T]) Add(f func(w *gateway.GatewayClient, ctx *gateway.EventContex
 	m.Funcs = append(m.Funcs, f)
 }
 
+// Builds the multi into a list of event handlers
+func (m *Multi[T]) Build() func(w *gateway.GatewayClient, ctx *gateway.EventContext, e *T) {
+	return func(w *gateway.GatewayClient, ctx *gateway.EventContext, e *T) {
+		for _, f := range m.Funcs {
+			f(w, ctx, e)
+		}
+	}
+}
+
 // Creates a new multi event handler for running multiple event handlers in a single event
 //
 // "We truly live in a world of wonders" - Tim Cook
 func NewMulti[T events.EventInterface](evts ...func(w *gateway.GatewayClient, ctx *gateway.EventContext, e *T)) *Multi[T] {
 	return &Multi[T]{
 		Funcs: evts,
-	}
-}
-
-func (m *Multi[T]) Build() func(w *gateway.GatewayClient, ctx *gateway.EventContext, e *T) {
-	return func(w *gateway.GatewayClient, ctx *gateway.EventContext, e *T) {
-		for _, f := range m.Funcs {
-			f(w, ctx, e)
-		}
 	}
 }
